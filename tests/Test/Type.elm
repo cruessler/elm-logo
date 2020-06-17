02bb675bb3f2eee7632f5027ee336a930f437c77
Christoph Rüßler <christoph.ruessler@mailbox.org>
2020-06-17 21:06:22 +0200

    Add test

---
+++ tests/Test/Type.elm
@@ -0,0 +1,37 @@
+module Test.Type exposing (toString)
+
+import Expect
+import Test exposing (Test, describe, test)
+import Vm.Type as T
+
+
+toString : Test
+toString =
+    describe "toString" <|
+        [ test "nested list" <|
+            \_ ->
+                let
+                    list =
+                        T.List
+                            [ T.List
+                                [ T.Word "small"
+                                , T.Word "medium"
+                                , T.Word "large"
+                                ]
+                            , T.List
+                                [ T.Word "vanilla"
+                                , T.List [ T.Word "ultra", T.Word "chocolate" ]
+                                , T.Word "lychee"
+                                , T.List [ T.Word "rum", T.Word "raisin" ]
+                                , T.Word "ginger"
+                                ]
+                            , T.List
+                                [ T.Word "cone"
+                                , T.Word "cup"
+                                ]
+                            ]
+                in
+                Expect.equal
+                    "[small medium large] [vanilla [ultra chocolate] lychee [rum raisin] ginger] [cone cup]"
+                    (T.toString list)
+        ]
