20903d3c83726e5fa30732bf402302e77aa76dec
Christoph Rüßler <christoph.ruessler@mailbox.org>
2021-01-01 19:56:51 +0100

    Extract test helper

--- tests/Test/Run.elm
+++ tests/Test/Run.elm
@@ -1,48 +1,10 @@
 module Test.Run exposing (..)
 
-import Environment.History exposing (Entry(..), History)
+import Environment.History exposing (Entry(..))
 import Expect
 import Logo
 import Test exposing (Test, describe, test)
-
-
-outputLines : List String -> History
-outputLines =
-    List.indexedMap (\i line -> ( i + 1, Output line ))
-
-
-printsLines : String -> List String -> Test
-printsLines program lines =
-    let
-        isOutput : ( Int, Entry ) -> Bool
-        isOutput entry =
-            case entry of
-                ( _, Output _ ) ->
-                    True
-
-                _ ->
-                    False
-    in
-    test program <|
-        \_ ->
-            let
-                logo =
-                    Logo.run program Logo.empty
-
-                history =
-                    Logo.getHistory logo
-
-                last =
-                    List.head history
-            in
-            case last of
-                Just ( _, Error message ) ->
-                    Expect.fail message
-
-                _ ->
-                    Expect.equal
-                        (lines |> outputLines |> List.reverse)
-                        (List.filter isOutput history)
+import Test.Helper exposing (printsLines)
 
 
 statements : Test
