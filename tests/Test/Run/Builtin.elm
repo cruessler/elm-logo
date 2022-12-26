85d22a9bee097ceddca99b8a5406349e011538a2
Christoph Rüßler <christoph.ruessler@mailbox.org>
2022-12-26 14:30:51 +0100

    Add describe blocks to primitives test

--- tests/Test/Run/Builtin.elm
+++ tests/Test/Run/Builtin.elm
@@ -35,52 +35,91 @@ commands =
 primitives : Test
 primitives =
     describe "primitives and their shorthands" <|
-        [ printsLine "print butfirst [ 1 1 ]" "1"
-        , printsLine "print bf [ 1 1 ]" "1"
-        , printsLine "print emptyp []" "true"
-        , printsLine "print empty? []" "true"
-        , printsLine "print first [ 1 ]" "1"
-        , printsLine "print integerp 1" "true"
-        , printsLine "print floatp 1.0" "true"
-        , printsLine "print wordp \"word" "true"
-        , printsLine "print wordp 1" "true"
-        , printsLine "print wordp 1.0" "true"
-        , printsLine "print wordp []" "false"
-        , printsLine "print equalp 1 1" "true"
-        , printsLine "print equal? 1 1" "true"
-        , printsLine "print notequalp 1 1" "false"
-        , printsLine "print notequal? 1 1" "false"
-        , printsLine "print minus 1" "-1"
-        , printsLine "print sum 1 1" "2"
-        , printsLine "print (sum 1 2 3)" "6"
-        , printsLine "print equalp (sum 1 2 3) 1 + 2 + 3" "true"
-        , printsLine "print difference 1 1" "0"
-        , printsLine "print product 1 1" "1"
-        , printsLine "print quotient 1 1" "1"
-        , printsLine "print round 1.2" "1"
-        , printsLine "print round 1.5" "2"
-        , printsLine "print round minus 1.5" "-2"
-        , printsLine "print round minus 1.8" "-2"
-        , printsLine "print greaterp 1 1" "false"
-        , printsLine "print greater? 1 1" "false"
-        , printsLine "print lessp 1 1" "false"
-        , printsLine "print less? 1 1" "false"
-        , printsLine "print remainder 1 1" "0"
-        , printsLine "print sentence 1 1" "1 1"
-        , printsLine "print se 1 1" "1 1"
-        , printsLine "print word 1 1" "11"
-        , printsLine "print char 65" "A"
-        , printsLine "print (bitand 7 5 4)" "4"
-        , printsLine "print bitand 7 minus 4" "4"
-        , printsLine "print bitnot 6" "-7"
-        , printsLine "print ashift 1 3" "8"
-        , printsLine "print ashift 8 minus 1" "4"
-        , printsLine "print lshift 1 3" "8"
-        , printsLine "print lshift 1 minus 2" "0"
-        , printsLine "print lshift 8 minus 1" "4"
-        , printsLine "print lshift 1 minus 32" "1"
-        , printsLine "print fput \"w \"ord" "word"
-        , printsLine "print fput \"w [ o r d ]" "w o r d"
+        [ describe "butfirst" <|
+            [ printsLine "print butfirst [ 1 1 ]" "1"
+            , printsLine "print bf [ 1 1 ]" "1"
+            ]
+        , describe "emptyp" <|
+            [ printsLine "print emptyp []" "true"
+            , printsLine "print empty? []" "true"
+            ]
+        , describe "first" <|
+            [ printsLine "print first [ 1 ]" "1" ]
+        , describe "integerp" <|
+            [ printsLine "print integerp 1" "true" ]
+        , describe "floatp" <|
+            [ printsLine "print floatp 1.0" "true" ]
+        , describe "wordp" <|
+            [ printsLine "print wordp \"word" "true"
+            , printsLine "print wordp 1" "true"
+            , printsLine "print wordp 1.0" "true"
+            , printsLine "print wordp []" "false"
+            ]
+        , describe "equalp" <|
+            [ printsLine "print equalp 1 1" "true"
+            , printsLine "print equal? 1 1" "true"
+            ]
+        , describe "notequalp" <|
+            [ printsLine "print notequalp 1 1" "false"
+            , printsLine "print notequal? 1 1" "false"
+            ]
+        , describe "minus" <|
+            [ printsLine "print minus 1" "-1" ]
+        , describe "sum" <|
+            [ printsLine "print sum 1 1" "2"
+            , printsLine "print (sum 1 2 3)" "6"
+            , printsLine "print equalp (sum 1 2 3) 1 + 2 + 3" "true"
+            ]
+        , describe "difference" <|
+            [ printsLine "print difference 1 1" "0" ]
+        , describe "product" <|
+            [ printsLine "print product 1 1" "1" ]
+        , describe "quotient" <|
+            [ printsLine "print quotient 1 1" "1" ]
+        , describe "round" <|
+            [ printsLine "print round 1.2" "1"
+            , printsLine "print round 1.5" "2"
+            , printsLine "print round minus 1.5" "-2"
+            , printsLine "print round minus 1.8" "-2"
+            ]
+        , describe "greaterp" <|
+            [ printsLine "print greaterp 1 1" "false"
+            , printsLine "print greater? 1 1" "false"
+            ]
+        , describe "lessp" <|
+            [ printsLine "print lessp 1 1" "false"
+            , printsLine "print less? 1 1" "false"
+            ]
+        , describe "remainder" <|
+            [ printsLine "print remainder 1 1" "0" ]
+        , describe "sentence" <|
+            [ printsLine "print sentence 1 1" "1 1"
+            , printsLine "print se 1 1" "1 1"
+            ]
+        , describe "word" <|
+            [ printsLine "print word 1 1" "11" ]
+        , describe "char" <|
+            [ printsLine "print char 65" "A" ]
+        , describe "bitand" <|
+            [ printsLine "print (bitand 7 5 4)" "4"
+            , printsLine "print bitand 7 minus 4" "4"
+            ]
+        , describe "bitnot" <|
+            [ printsLine "print bitnot 6" "-7" ]
+        , describe "ashift" <|
+            [ printsLine "print ashift 1 3" "8"
+            , printsLine "print ashift 8 minus 1" "4"
+            ]
+        , describe "lshift" <|
+            [ printsLine "print lshift 1 3" "8"
+            , printsLine "print lshift 1 minus 2" "0"
+            , printsLine "print lshift 8 minus 1" "4"
+            , printsLine "print lshift 1 minus 32" "1"
+            ]
+        , describe "fput" <|
+            [ printsLine "print fput \"w \"ord" "word"
+            , printsLine "print fput \"w [ o r d ]" "w o r d"
+            ]
         ]
 
 
